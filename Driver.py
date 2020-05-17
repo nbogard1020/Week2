aaa4509f50bd6db3ba0a57c06e943d49b2628388
nbogard1020 <65126053+nbogard1020@users.noreply.github.com>
2020-05-17 14:10:38 -0500

    Add files via upload

---
+++ Driver.py
@@ -0,0 +1,11 @@
+from CheckingAccount import CheckingAccount
+
+Account = CheckingAccount("Nicole","Saint Louis",1234, 26)
+
+Account.credtitAccount(10.10)
+
+Account.debitAccount(20.20)
+
+Account.debitAccount(30.30)
+
+Account.showBalance()
